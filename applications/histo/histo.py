6fcecb2148f958a4c0905c1c22a76b9fb886fcc8
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-07-09 16:56:18 -0700

    Implemented the rest of the applications.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,32 @@
 # Your code here
+ignored_characters = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
 
+
+def histogram():
+    counts = dict()
+    with open("histo/robin.txt") as f:
+        words = f.read()
+        split_words = words.split()
+
+    for word in split_words:
+        histo = ""
+        for char in word:
+            if char not in ignored_characters:
+                histo += char
+        word = histo.lower()
+
+        if word in counts:
+            counts[word] += 1
+        elif word == "" or word == " ":
+            break
+        else:
+            counts[word] = 1
+
+    items = list(counts.items())
+    items.sort(key = lambda e: e[1], reverse = True)
+    counts = (dict(items))
+    for (string, value) in counts.items():
+        print(f'{string} {" " * (15 - len(string))} {"#" * value}')
+
+
+print(histogram())
